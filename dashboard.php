fd1c2ee599c42dc932a86c57d769c62002473317
bk-coding <125408986+bk-coding@users.noreply.github.com>
2024-03-15 23:36:52 +0100

    #

--- dashboard.php
+++ dashboard.php
@@ -2,8 +2,8 @@
 
 <div class="bodycontent">
     <div class="category">
-        <a name="divers" href="#"><div class="bouton"><i class="fa-solid fa-snowflake"></i><br /><br />Divers</div></a>
-        <a name="autres" href="#"><div class="bouton"><i class="fa-brands fa-github"></i><br /><br />Autres</div></a>
+        <a name="divers" href="#"><div class="bouton"><div><i class="fa-solid fa-snowflake"></i></div><div>Divers</div></div></a>
+        <a name="autres" href="#"><div class="bouton"><div><i class="fa-brands fa-github"></i></div><div>Autres</div></div></a>
     </div>
     <div class="category">
         <p>lorem ipsum</p>

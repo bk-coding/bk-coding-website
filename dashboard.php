905a8258349e9f6b398ba79d2a2bb072faea954f
bk-coding <125408986+bk-coding@users.noreply.github.com>
2024-03-16 00:56:41 +0100

    #

--- dashboard.php
+++ dashboard.php
@@ -6,9 +6,10 @@
         <a name="divers" href="#"><div class="bouton"><div><i class="fa-solid fa-snowflake"></i></div><div>Divers</div></div></a>
         <a name="autres" href="#"><div class="bouton"><div><i class="fa-brands fa-github"></i></div><div>Autres</div></div></a>
     </fieldset>
-    <div class="category">
+    <fieldset class="category">
+        <legend>Autres</legend>
         <p>lorem ipsum</p>
-    </div>
+    </fieldset>
 </div>
 
 <?php include('parts/footer.php'); ?>

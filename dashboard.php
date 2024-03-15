1b818f6b6215e567876095a524b3be0d1ed8ded3
bk-coding <125408986+bk-coding@users.noreply.github.com>
2024-03-15 15:12:37 +0100

    #

--- dashboard.php
+++ dashboard.php
@@ -1,8 +1,8 @@
 <?php $title = "dashboard"; include('parts/header.php'); ?>
 
 <div class="bodycontent">
-    <button type="button" class="carre" name="icecoder">ICEcoder</button>
-    <button type="button" class="carre" name="autres">...</button>
+    <a class="carre" name="icecoder" href="ICEcoder"><i class="fa-solid fa-snowflake"></i>ICEcoder</a>
+    <a class="carre" name="autres" href="#">...</a>
 </div>
 
 <?php include('parts/footer.php'); ?>

f3649e6b8dc6b9ccc9fcfad6cd4041d478dee196
bk-coding <125408986+bk-coding@users.noreply.github.com>
2024-03-15 16:47:08 +0100

    #

--- dashboard.php
+++ dashboard.php
@@ -1,8 +1,8 @@
 <?php $title = "dashboard"; include('parts/header.php'); ?>
 
 <div class="bodycontent">
-    <div class="bouton"><a name="icecoder" href="https://bk-coding.net/ICEcoder" target="_blank"><i class="fa-solid fa-snowflake"></i><br>ICEcoder</a></div>
-    <div class="bouton"><a name="autres" href="#">...</a></div>
+    <div class="bouton"><a name="icecoder" href="https://bk-coding.net/ICEcoder" target="_blank"><p><i class="fa-solid fa-snowflake"></i><br>ICEcoder</p></a></div>
+    <div class="bouton"><a name="autres" href="#"><p>...</p></a></div>
 </div>
 
 <?php include('parts/footer.php'); ?>

07a4fef1984223f25cfb275996af724fa6d5059f
bk-coding <125408986+bk-coding@users.noreply.github.com>
2024-03-15 16:31:45 +0100

    #

--- parts/header.php
+++ parts/header.php
@@ -16,6 +16,10 @@ $titlea = $lang[$title];
 		<title><?= $lang["sitename"]." - ".$titlea; ?></title>
 		<!-- Style CSS -->
 		<link rel="stylesheet" type="text/css" href="style.css">
+		<link rel="apple-touch-icon" sizes="180x180" href="favicon/apple-touch-icon.png">
+		<link rel="icon" type="image/png" sizes="32x32" href="favicon/favicon-32x32.png">
+		<link rel="icon" type="image/png" sizes="16x16" href="favicon/favicon-16x16.png">
+		<link rel="manifest" href="favicon/site.webmanifest">
 		<script src="https://kit.fontawesome.com/766d810226.js" crossorigin="anonymous"></script>
 	</head>
 	<body>

96f142ffd9a0d7aaa01899e46061b41f7a87a22b
bk-coding <125408986+bk-coding@users.noreply.github.com>
2024-03-16 00:54:42 +0100

    #

--- dashboard.php
+++ dashboard.php
@@ -1,13 +1,11 @@
 <?php $title = "dashboard"; include('parts/header.php'); ?>
 
 <div class="bodycontent">
-    <div class="category">
-        <fieldset>
-            <legend>Applis</legend>
-            <a name="divers" href="#"><div class="bouton"><div><i class="fa-solid fa-snowflake"></i></div><div>Divers</div></div></a>
-            <a name="autres" href="#"><div class="bouton"><div><i class="fa-brands fa-github"></i></div><div>Autres</div></div></a>
-        </fieldset>
-    </div>
+    <fieldset class="category">
+        <legend>Applis</legend>
+        <a name="divers" href="#"><div class="bouton"><div><i class="fa-solid fa-snowflake"></i></div><div>Divers</div></div></a>
+        <a name="autres" href="#"><div class="bouton"><div><i class="fa-brands fa-github"></i></div><div>Autres</div></div></a>
+    </fieldset>
     <div class="category">
         <p>lorem ipsum</p>
     </div>

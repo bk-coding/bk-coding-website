2c6002b8f10ba3e3e84b1a7a24340fb0e5ee44ff
bk-coding <125408986+bk-coding@users.noreply.github.com>
2024-03-16 01:20:10 +0100

    #

--- dashboard.php
+++ dashboard.php
@@ -4,8 +4,10 @@ include('parts/header.php'); ?>
 <div class="bodycontent">
     <fieldset class="category">
         <legend>Applis</legend>
-        <a name="divers" target="_blank" href="https://hpanel.hostinger.com/websites/bk-coding.net"><div class="bouton"><div><i class="fa-solid fa-server"></i></div><div>Hostinger</div></div></a>
-        <a name="autres" target="_blank" href="https://github.com/bk-coding"><div class="bouton"><div><i class="fa-brands fa-github"></i></div><div>GitHub</div></div></a>
+        <a name="hostinger" target="_blank" href="https://hpanel.hostinger.com/websites/bk-coding.net"><div class="bouton"><div><i class="fa-solid fa-server"></i></div><div>Hostinger</div></div></a>
+        <a name="github" target="_blank" href="https://github.com/bk-coding"><div class="bouton"><div><i class="fa-brands fa-github"></i></div><div>GitHub</div></div></a>
+        <a name="fontawesome" target="_blank" href="https://fontawesome.com/search"><div class="bouton"><div><i class="fa-solid fa-font-awesome"></i></div><div>FontAwesome</div></div></a>
+        <a name="mdn" target="_blank" href="https://developer.mozilla.org/fr/docs/Web#r%C3%A9f%C3%A9rences_des_technologies_web"><div class="bouton"><div><i class="fa-solid fa-book-bookmark"></i></div><div>MDN</div></div></a>
     </fieldset>
     <fieldset class="category">
         <legend>Autres</legend>

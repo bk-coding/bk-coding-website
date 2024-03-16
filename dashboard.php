29806c912cc0e076b0ba8922a4d6580c4aed805d
bk-coding <125408986+bk-coding@users.noreply.github.com>
2024-03-16 01:02:13 +0100

    lien

--- dashboard.php
+++ dashboard.php
@@ -1,10 +1,11 @@
-<?php $title = "dashboard"; include('parts/header.php'); ?>
+<?php $title = "dashboard";
+include('parts/header.php'); ?>
 
 <div class="bodycontent">
     <fieldset class="category">
         <legend>Applis</legend>
         <a name="divers" href="#"><div class="bouton"><div><i class="fa-solid fa-snowflake"></i></div><div>Divers</div></div></a>
-        <a name="autres" href="#"><div class="bouton"><div><i class="fa-brands fa-github"></i></div><div>Autres</div></div></a>
+        <a name="autres" href="https://github.com/bk-coding"><div class="bouton"><div><i class="fa-brands fa-github"></i></div><div>GitHub</div></div></a>
     </fieldset>
     <fieldset class="category">
         <legend>Autres</legend>

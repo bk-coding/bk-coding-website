d78bcd829b8132bc4e6dcf82fce990809665c1e7
bk-coding <125408986+bk-coding@users.noreply.github.com>
2024-03-29 22:38:03 +0100

    #

--- intermittent.php
+++ intermittent.php
@@ -38,7 +38,7 @@ $cachets = $pdo->query("SELECT * FROM cachets")->fetchAll();
         <div><label for="nombre_cachet"> Nombre de cachet : </label><input type="number" name="nombre_cachet" id="nombre_cachet" required></div>
         <div><label for="montant_brut"> Montant Brut : </label><input type="number" name="montant_brut" id="montant_brut" required></div>
         <div><label for="montant_net"> Montant Net : </label><input type="number" name="montant_net" id="montant_net" required></div>
-        <div><label for="description"> Description : </label><input type="text" name="description" id="description"></div>
+        <div><label for="description"> Description : </label><input type="text" name="description" id="description"></div><br />
         <div><input type="submit" value="Enregistrer"></div>
     </form>
 </fieldset>

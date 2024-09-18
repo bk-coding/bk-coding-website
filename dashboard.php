0c5c1ea35da79511ca5c9521ab5096d0346edd7b
bk-coding <dev@bk-coding.net>
2024-09-18 15:34:09 +0200

    #

--- dashboard.php
+++ dashboard.php
@@ -13,7 +13,7 @@ include('parts/header.php'); ?>
     $sections = [
         "Applis" => [
             ["name" => "hostinger", "target" => "_blank", "href" => "https://hpanel.hostinger.com/websites/bk-coding.net", "icon" => "fa-server", "label" => "Hostinger"],
-            ["name" => "github", "target" => "_blank", "href" => "https://github.com/bk-coding", "icon" => "fa-github", "label" => "GitHub"],
+            ["name" => "github", "target" => "_blank", "href" => "https://github.com/bk-coding", "icon" => "fa-brands fa-github", "label" => "GitHub"],
             ["name" => "fontawesome", "target" => "_blank", "href" => "https://fontawesome.com/search", "icon" => "fa-font-awesome", "label" => "FontAwesome"],
             ["name" => "mdn", "target" => "_blank", "href" => "https://developer.mozilla.org/fr/docs/Web#r%C3%A9f%C3%A9rences_des_technologies_web", "icon" => "fa-book-bookmark", "label" => "MDN"]
         ],

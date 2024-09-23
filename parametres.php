faf7c20f57d365e214411fa12b7e9e4471a0a7be
bk-coding <dev@bk-coding.net>
2024-09-23 23:05:52 +0200

    #

--- parametres.php
+++ parametres.php
@@ -111,14 +111,14 @@ $liens = $stmt->fetchAll();
 </div>
 
 <script>
-    function editLien($lien) {
-        document.getElementById('lienId').value = $lien.id;
-        document.getElementById('type_section').value = $lien.type_section;
-        document.getElementById('nom_interne').value = $lien.nom_interne;
-        document.getElementById('cible').value = $lien.cible;
-        document.getElementById('adresse_lien').value = $lien.adresse_lien;
-        document.getElementById('icon').value = $lien.icon;
-        document.getElementById('titre_bouton').value = $lien.titre_bouton;
+    function editLien(lien) {
+        document.getElementById('lienId').value = lien.id;
+        document.getElementById('type_section').value = lien.type_section;
+        document.getElementById('nom_interne').value = lien.nom_interne;
+        document.getElementById('cible').value = lien.cible;
+        document.getElementById('adresse_lien').value = lien.adresse_lien;
+        document.getElementById('icon').value = lien.icon;
+        document.getElementById('titre_bouton').value = lien.titre_bouton;
         }
     function afficheCat(bouton) {
         // Vérifie si l'élément existe

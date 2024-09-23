ec95d6152b307557e92b9f92f4054b244635d268
bk-coding <dev@bk-coding.net>
2024-09-24 01:18:05 +0200

    #

--- hashmdp.php
+++ hashmdp.php
@@ -13,15 +13,8 @@ include('parts/header.php');
 <fieldset class="categoryajout">
     <legend>Crypter un mot de passe</legend>
     <form method="post">
-        <input type="text" name="password" placeholder="Entrer votre mot de passe à crypter :">
+        <input type="text" name="password" placeholder="Entrer votre mot de passe">
         <input type="submit" value="Crypter">
-    </form>
-</fieldset>
-<fieldset class="categoryajout">
-    <legend>Décrypter un mot de passe</legend>
-    <form method="post">
-        <input type="text" name="decrypt" placeholder="Entrer votre mot de passe à décrypter :">
-        <input type="submit" value="Décrypter">
     </form>
 </fieldset>
 
@@ -31,11 +24,6 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         $password = $_POST["password"];
         $hashedPassword = password_hash($password, PASSWORD_DEFAULT);
         echo "<script>alert('Votre mot de passe haché est : " . json_encode($hashedPassword) . "');</script>";
-    }
-    elseif (isset($_POST['decrypt'])) {
-        $decrypt = $_POST['decrypt'];
-        $decryptPassword = password_get_info($decrypt);
-        echo "<script>alert('Votre mot de passe décrypté est : ".json_encode($decryptPassword)."');</script>";
     }
     else {
         echo "<script>alert('Veuillez entrer un mot de passe.');</script>";

dd2f50121fed58fc4a51c1976c43f65592d1f6e8
bk-coding <dev@bk-coding.net>
2024-09-25 18:08:34 +0200

    #

--- dashboard.php
+++ dashboard.php
@@ -5,6 +5,10 @@ author: Bastien Kilian
  -->
 
 <?php
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
 $title = "dashboard";
 include('parts/header.php');
 require_once('dbconfig.php');
--- parts/load_messages.php
+++ parts/load_messages.php
@@ -5,9 +5,8 @@ require_once('./dbconfig.php');
 // Récupérer tous les messages
 $stmt = $pdo->prepare("SELECT username, message, timestamp FROM messages ORDER BY timestamp DESC");
 $stmt->execute();
-$result = $stmt->fetchAll();
 // Affichage des messages
-$result = $stmt->fetchAll(); // Récupérez toutes les lignes
+$result = $stmt->fetchAll();
 if (count($result) > 0) {
     foreach ($result as $row) {
         echo "<div class='message'><strong>" . htmlspecialchars($row['username']) . ":</strong> " . htmlspecialchars($row['message']) . " <em>(" . date('Y-m-d H:i:s', strtotime($row['timestamp'])) . ")</em></div>";

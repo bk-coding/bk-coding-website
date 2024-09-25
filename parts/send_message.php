5ff36742b1035cb6ab782aca4e97d90897e4090e
bk-coding <dev@bk-coding.net>
2024-09-25 19:59:04 +0200

    #

--- parts/send_message.php
+++ parts/send_message.php
@@ -3,15 +3,8 @@
 try {
     require_once('../dbconfig.php');
 
-    function clean_message($message)
-    {
-        // Autoriser certains caractères spéciaux, mais désinfecter les autres
-        return htmlspecialchars($message, ENT_QUOTES, 'UTF-8'); // Pour échapper les guillemets également
-    }
-
-    // Préparer et exécuter la requête d'insertion
-    $username = htmlspecialchars($_POST['username']);
-    $message = clean_message($_POST['message']);
+    $username = $_POST['username'];
+    $message = $_POST['message'];
 
     $stmt = $pdo->prepare("INSERT INTO messages (username, message) VALUES (?, ?)");
     $stmt->execute([$username, $message]);

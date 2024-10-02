35cd2222085e7d593c2742da48ea543e091a722d
bk-coding <dev@bk-coding.net>
2024-10-02 12:59:37 +0200

    #

--- parts/load_messages.php
+++ parts/load_messages.php
@@ -12,7 +12,7 @@ try {
 
     if (count($result) > 0) {
         foreach ($result as $row) {
-            echo "<div class='message'><em>(" . date('d-m-Y, H:i', strtotime($row['timestamp'])) . ")</em><strong>" . htmlspecialchars($row['username']) . ":</strong> " . htmlspecialchars($row['message'], ENT_QUOTES) . "</div>";
+            echo "<div class='message'><em>(" . date('d-m-Y, H:i', strtotime($row['timestamp'])) . ")</em> <strong>" . htmlspecialchars($row['username']) . " :</strong> " . htmlspecialchars($row['message'], ENT_QUOTES) . "</div>";
         }
     } else {
         echo "Aucun message pour l'instant.";

0400916fbc6264547b53d510be1aeb79ed23c573
bk-coding <dev@bk-coding.net>
2024-10-02 19:32:41 +0200

    update dashboard.php and style.css

--- dashboard.php
+++ dashboard.php
@@ -19,7 +19,7 @@ session_start();
         </div>
         <form id="chat-form">
             <input type="hidden" id="username" value="<?php echo $_SESSION['username']; ?>" required>
-            <input type="text" id="message" placeholder="Votre message" required>
+            <input type="text" id="message" spellcheck="true" size="50" placeholder="Votre message" required>
             <button type="submit">Envoyer</button>
         </form>
     </fieldset>
